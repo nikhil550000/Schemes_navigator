e168a63771af2799d24eb7e983e820b44f91bd57
nikhil550000 <nikhilsai550000.com>
2025-04-20 14:58:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Scheme navigator Chatbot',
+    version= '0.0.0',
+    author= 'nikhil',
+    author_email= 'nikhil550000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
